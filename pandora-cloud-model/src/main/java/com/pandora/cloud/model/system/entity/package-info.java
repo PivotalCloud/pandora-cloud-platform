b81038a28389b01f506dc62f5741c983252d37fc
marklin <marklin1992@outlook.com>
2021-04-29 18:03:15 +0800

    基础模块修改

---
+++ pandora-cloud-model/src/main/java/com/pandora/cloud/model/system/entity/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>封装Qicloud项目package-info类.<br></p>
+ * <p>//TODO...<br></p>
+ *
+ * @author Powered by marklin 2021-04-29 17:05
+ * @version 1.0.0
+ * <p>Copyright © 2018-2021 Pivotal Cloud Technology Systems Incorporated. All rights reserved.<br></p>
+ */
+package com.pandora.cloud.model.system.entity;
