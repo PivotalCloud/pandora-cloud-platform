61e1199612ac468e330c5fc4e1b9f072842b48ed
marklin <marklin1992@outlook.com>
2021-04-30 00:32:37 +0800

    基础模块修改

---
+++ pandora-cloud-undertow/src/main/java/com/pandora/cloud/undertow/PandoraUndertowApplication.java
@@ -0,0 +1,20 @@
+package com.pandora.cloud.undertow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>封装Qicloud项目PandoraUndertowApplication类.<br></p>
+ * <p>//TODO...<br></p>
+ *
+ * @author Powered by marklin 2021-04-29 20:52
+ * @version 1.0.0
+ * <p>Copyright © 2018-2021 Pivotal Cloud Technology Systems Incorporated. All rights reserved.<br></p>
+ */
+@SpringBootApplication
+public class PandoraUndertowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandoraUndertowApplication.class, args);
+    }
+}
